8b0dbcac35f868dd805ad54910f9186c1bb5572c
Lautaro Muiña <87960189+muinalautaro16@users.noreply.github.com>
2025-02-26 01:52:36 -0300

    + Cambios en los numeros telefonicos

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ import Footer from "../components/Footer";
 import useFnWspp from "../hooks/useFnWspp";
 
 const Home = () => {
-  const {link, getRandomItem} = useFnWspp(['https://wa.me/525522696119', 'https://wa.me/525522696119']);
+  const {link, getRandomItem} = useFnWspp(['https://wa.me/5492954201374', 'https://wa.me/5492954201374']);
   return (
     <div className="flex flex-col min-h-screen">
       <Nav />
